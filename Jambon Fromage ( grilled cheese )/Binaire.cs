ee0ac27d2b22653f974e1498f271c29fa8dbf110
TerminaPaul <paul.ull.leathers@gmail.com>
2026-06-20 17:40:38 +0200

    fix

--- Jambon Fromage ( grilled cheese )/Binaire.cs
+++ Jambon Fromage ( grilled cheese )/Binaire.cs
@@ -54,8 +54,8 @@ public class Binaire
         string message = nbErreur switch
         {
             1 => "la chaine de caractere est null",
-            2 => "la chaine de caractere (binaire) a moins de 8 octets",
-            3 => "la chaine de caractere (binaire) a plus de 8 octets",
+            2 => "la chaine de caractere (binaire) a moins de 8 bits",
+            3 => "la chaine de caractere (binaire) a plus de 8 bits",
             4 => "la chaine de caractere n'est pas binaire",
             5 => "la chaine de caractere (Caractere) a plus que 1 caractere",
             _ => "inconnue"
